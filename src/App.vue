d74a6b70e4448cf3b6e5944cf48c56469af5e91a
advtek02.tn <advtek02.tn@innolux.com>
2026-01-06 15:18:54 +0800

    移除 KuoFund 程式

--- src/App.vue
+++ src/App.vue
@@ -41,22 +41,6 @@
         funButtons: [],
         imagenOption: {},
     });
-    // 郭家基金 - 功能設定
-    let kf_funSetting = reactive({
-        bank_info: {},
-        file_info: {},
-        member: {},
-        activity_annouce_period: {
-            start: "",
-            end: "",
-        },
-        agent: {
-            name: "",
-            phone: "",
-            lineID: "",
-            term:"",
-        },
-    });
     // 選擇的 component
     let selComponentName = ref("");
     // 登入狀態
@@ -188,17 +172,6 @@
                 // appSetting.googleMapApiKey
                 appSetting.googleMapApiKey = appSettingObj["google_map_api_key"];
 
-                // 郭家基金 - 功能設定值
-                kf_funSetting.bank_info = appSettingObj["kf_bank_info"];
-                kf_funSetting.file_info = appSettingObj["kf_file_info"];
-                kf_funSetting.member = appSettingObj["kf_member"];
-                kf_funSetting.activity_annouce_period.start = appSettingObj["kf_activity_annouce_period"]["value1"];
-                kf_funSetting.activity_annouce_period.end = appSettingObj["kf_activity_annouce_period"]["value2"];
-                kf_funSetting.agent.name = appSettingObj["agent_name"];
-                kf_funSetting.agent.phone = appSettingObj["agent_phone"];
-                kf_funSetting.agent.lineID = appSettingObj["agent_line_id"];
-                kf_funSetting.agent.term = appSettingObj["agent_term"];
-
                 // appSetting.reference
                 {
                     appSetting.reference = [];
@@ -248,36 +221,17 @@
                 {
                     appSetting.funButtons.splice(0, appSetting.funButtons.length);
 
-                    let allFunctionKeys = ["finance", "chat", "imagen", "plan_trip", "trip_schedule", "footmark", "recollection", "achievement", "finance_kf", "rule_kf", "activity_kf", "recall", "qa"];
+                    let allFunctionKeys = ["finance", "chat", "imagen", "plan_trip", "trip_schedule", "footmark", "recollection", "achievement"];
                     let buildingFunctionKeys = [];
-                    let buildingFunctionKeys_kf = [];
                     allFunctionKeys.forEach((funKey, fk_i) => {
                         if(userInfo.funcs.indexOf(funKey) >= 0){
-
-                            if(buildingFunctionKeys.indexOf(funKey) >= 0 && userInfo.role === "admin"){
-                                appSetting.funButtons.push({ key: funKey, display_text: userInfo.languages[funKey] });
-                            }                            
-                            if(buildingFunctionKeys_kf.indexOf(funKey) >= 0 && userInfo.role === "admin_kf"){
-                               appSetting.funButtons.push({ key: funKey, display_text: userInfo.languages[funKey] });
-                            }
-                            if(buildingFunctionKeys.indexOf(funKey) < 0 && buildingFunctionKeys_kf.indexOf(funKey) < 0){
-                               appSetting.funButtons.push({ key: funKey, display_text: userInfo.languages[funKey] });
-                            }
+                            appSetting.funButtons.push({ key: funKey, display_text: userInfo.languages[funKey] });
                         }
                     });
                 }
 
-                if(userInfoObj["account"].toUpperCase() === "KUOFAMILY" || userInfoObj["role"] === "admin_kf"){
-                    // 郭家基金成員 - 預設開啟基金明細 component
-                    appSetting.funButtons.forEach((fbObj, fb_i) => {
-                        if(fbObj["key"] === "finance_kf"){
-                            gotoPage(fbObj);
-                        }
-                    });
-                }else{
-                    // 預設開啟 gallery component
-                    gotoPage("gallery");
-                }
+                // 預設開啟 gallery component
+                gotoPage("gallery");
             });
         }
     }
