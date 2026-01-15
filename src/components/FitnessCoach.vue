8bc19f2d86e20075fafdbd61f5080b944c88dc57
advtek02.tn <advtek02.tn@innolux.com>
2026-01-15 16:35:18 +0800

    完善功能

--- src/components/FitnessCoach.vue
+++ src/components/FitnessCoach.vue
@@ -30,47 +30,8 @@
     let messages = reactive([]);
     let userInfo = reactive({});
     let aiRole = reactive({});
-    // 提詞機 - 選項
-    let promptOptions = reactive({
-        nation: [
-            { value: "global", text: "國際", },
-            { value: "taiwan", text: "台灣", },
-            { value: "usa", text: "美國", },
-            { value: "japan", text: "日本", },
-            { value: "korea", text: "韓國", },
-        ],
-        scope: [
-            { value: "entertainment", text: "演藝圈", },
-            { value: "car", text: "汽車", },
-            { value: "motorcycle", text: "機車", },
-            { value: "stock", text: "股市", },
-            { value: "house", text: "房地產", },
-            { value: "politic", text: "政治", },
-            { value: "basketball", text: "籃球", },
-            { value: "baseball", text: "棒球", },
-        ],
-        time: [
-            { value: "today", text: "今天", },
-            { value: "24hours", text: "過去24小時", },
-            { value: "1week", text: "近一週", },
-            { value: "1month", text: "近一個月", },
-            { value: "3month", text: "近三個月", },
-            { value: "6month", text: "近半年", },
-            { value: "1year", text: "近一年", },
-        ],
-        type: [
-            { value: "news", text: "新聞", },
-            { value: "business", text: "市場", },
-        ],
-    });
-    // 提詞機 - 選擇
-    let promptAction = ref("summary");
-    let promptNation = ref("taiwan");
-    let promptScope = ref("entertainment");
-    let promptTime = ref("6month");
-    let promptType = ref("news");
-    // 提詞機 - 結果
-    let prompt = ref("");
+
+    let planName = ref("");
 
     // 初始化 component
     function init(){
@@ -82,9 +43,6 @@
         fetchInitData();
         userMessage.value = "Hi";
         chat();
-
-        // 先組合出預設 prompt
-        combinePrompt();
     }
     // 取得初始資料
     function fetchInitData(){
@@ -115,7 +73,7 @@
         }
 
         let chatPromise = fetchData({
-            api: "chat",
+            api: "coach",
             data: {
                 account: props.account,
                 chat_room_uuid: chat_room_uuid.value,
@@ -175,79 +133,84 @@
             });
         });
     }
-    // translate with ai
-    function translate(language, message){
-        console.log("translate.language=" + language);
-        console.log("translate.message=" + message);
-
-        // 關閉全部 modal
+    // 統整訓練計畫
+    function sumupPlan(){
+        // 關閉全部 Modal
         closeAllModal();
-        
-        chatState.value = "TALKING";
-        {
-            // 讓 app scroll 到底
-            let chatBoxElement = document.getElementById("chatBox");
-            chatBoxElement.scrollTop = chatBoxElement.scrollHeight;
-        }
 
-        let translatePromise = fetchData({
-            api: "translate",
+        // 整理聊天內容成"json"
+        let sumupPromise = fetchData({
+            api: "sumup_plan",
             data: {
-                language: language,
-                message: message,
+                account: props.account,
+                chat_room_uuid: chat_room_uuid.value,
+                message: "",
+                time: moment().format("YYYY-MM-DD HH:mm:ss"),
             }
         }, "AI");
-        Promise.all([translatePromise]).then((values) => {
-            console.log("translatePromise.values=", values);
+        Promise.all([sumupPromise]).then((values) => {
+            console.log("sumupPromise.values=", values);
 
             let ai_msg = values[0]["message"];
-            let ai_role = "AI_TRANSLATE";
-            let speaker = "TRANSLATE";
-            let short_name = "T";
+            ai_msg = ai_msg.replace(/```/g, "").replace(/json/g, "").replace(/\n/g, "").trim();
+            //console.log("ai_msg=", ai_msg);
+            let planList = JSON.parse(ai_msg);
+            console.log("planList=", planList);
 
-            // AI 出錯了
-            if(ai_msg.indexOf("ERROR:") === 0){
-                if(ai_msg.indexOf("ERROR:429 RESOURCE_EXHAUSTED") === 0){
-                    emit('popupMessage', false, "AI 忙碌中... 請稍等再聊..."); // Emitting the event with data
-                    ai_msg = "AI 忙碌中... 請稍等再聊...";
-                }else{
-                    emit('popupMessage', false, ai_msg); // Emitting the event with data
-                }
+            // 新增訓練菜單
+            newFitnessPlan(planList);
+        });
+    }
+    // 新增訓練菜單
+    function newFitnessPlan(planList){
+        console.log("newFitnessPlan.planList=", planList);
 
-                ai_role = "AI_ERROR";
-                speaker = "ERROR";
-                short_name = "E";
-
-            }else{
-                // 讓 user 可以直接重傳的機制
-                for(let msg_i = messages.length -1; msg_i >= 0; msg_i--){
-                    if(messages[msg_i] && messages[msg_i]["role"] === "user"){
-                        // AI 正確回覆, 視為"訊息傳遞成功"
-                        messages[msg_i]["sent_success"] = true; 
-                        break;
-                    }
-                }
-            }
-
-            messages.push({
-                role: ai_role,
-                speaker: speaker,
-                short_name: short_name,
-                message: ai_msg,
-                time: moment().format("HH:mm:ss"),
-            });
-
-            // Vue3 因資料改變 DOM 後觸發
-            nextTick(() => {
-                chatState.value = "DONE";
-                userMessage.value = "";
-
-                // 讓 app scroll 到底
-                let chatBoxElement = document.getElementById("chatBox");
-                chatBoxElement.scrollTop = chatBoxElement.scrollHeight;
-            });
+        let purposes = [];
+        planList.forEach((planObj, m_i) => {
+            purposes.push( planObj["purpose"] );
         });
 
+        let newPlanPromise = fetchData({
+            api: "new_fitness_plan",
+            data: {
+                account: props.account,
+                purposes: purposes,
+                plan_name: planName.value,
+            }
+        });
+        Promise.all([newPlanPromise]).then((values) => {
+            console.log("newPlanPromise.values=", values);
+            let plan_no = values[0]["message"].split("=")[1];
+
+            // 新增訓練菜單-細項
+            let promiseList_newItem = [];
+            planList.forEach((planObj, m_i) => {
+                planObj["item"].forEach((miObj, mi_i) => {
+                    let planItemObj = {
+                        plan_no: plan_no,
+                        day_seq: m_i,
+                        item_seq: mi_i,
+                        name: miObj["name"],
+                        frequency: miObj["frequency"],
+                        purpose: miObj["purpose"],
+                    };
+                    promiseList_newItem.push( newFitnessPlanItem(planItemObj) );
+                });
+            });
+            Promise.all(promiseList_newItem).then((values_item) => {
+                console.log("promiseList_newItem.values_item=", values_item);
+
+
+            });
+        });
+    }
+    // 新增訓練菜單-細項
+    function newFitnessPlanItem(planItemObj){
+        let newPlanItemPromise = fetchData({
+            api: "new_fitness_plan_item",
+            data: planItemObj
+        });
+        return newPlanItemPromise;
     }
     // 送出 message
     function send(){
@@ -277,78 +240,6 @@
         userMessage.value = re_msg;
         send();
     }      
-    // 開啟 prompt modal
-    function openPromptModal(){
-        document.getElementById("promptModal").showModal();
-    }
-    // 組合提詞
-    function combinePrompt(){
-        let c_prompt = "幫我整理以下資料\n";
-        /*
-        console.log("combinePrompt.promptNation=", promptNation.value);
-        console.log("combinePrompt.promptScope=", promptScope.value);
-        console.log("combinePrompt.promptTime=", promptTime.value);
-        console.log("combinePrompt.promptType=", promptType.value);
-        */
-        if(promptTime.value){
-            promptOptions.time.forEach((timeObj, act_i) => {
-                if(promptTime.value === timeObj.value){
-                    let today = moment().format("YYYY-MM-DD");
-                    let stDate = "";
-
-                    switch(promptTime.value){
-                        case "today": stDate = today; break;
-                        case "24hours": stDate = moment().add(-1, "d").format("YYYY-MM-DD"); break;
-                        case "1week": stDate = moment().add(-7, "d").format("YYYY-MM-DD"); break;
-                        case "1month": stDate = moment().add(-1, "M").format("YYYY-MM-DD"); break;
-                        case "3month": stDate = moment().add(-3, "M").format("YYYY-MM-DD"); break;
-                        case "6month": stDate = moment().add(-6, "M").format("YYYY-MM-DD"); break;
-                        case "1year": stDate = moment().add(-12, "M").format("YYYY-MM-DD"); break;
-                    }
-                    c_prompt += "<時間>" + stDate + "~" + today + "</時間>" + "\n";
-                }
-            });
-        }
-
-        if(promptNation.value){
-            promptOptions.nation.forEach((nationObj, act_i) => {
-                if(promptNation.value === nationObj.value){
-                    c_prompt += "<國家>" + nationObj.text + "</國家>" + "\n";
-                }
-            });
-        }
-
-        c_prompt += "<資料主題>";
-        promptOptions.scope.forEach((scopeObj, act_i) => {
-            if(promptScope.value === scopeObj.value){
-                c_prompt += scopeObj.text;
-            }
-        });
-        promptOptions.type.forEach((typeObj, act_i) => {
-            if(promptType.value === typeObj.value){
-                c_prompt += typeObj.text;
-            }
-        });
-        c_prompt += "</資料主題>" + "\n";
-
-        prompt.value = c_prompt;
-    }
-    // 傳送提詞
-    function sendPrompt(){
-        if(prompt.value){
-            chat_room_uuid.value = "INIT";
-            userMessage.value = prompt.value;
-            send();
-        }else{
-            let error_msg = "你必須提供點內容, 不然提詞機要怎麼幫你轉送~~";
-            emit('popupMessage', false, error_msg); // Emitting the event with data
-        }
-        closePromptModal();
-    }
-    // 關閉 prompt modal
-    function closePromptModal(){
-        document.getElementById("promptModal").close();
-    }
     // 開啟 NewChat 再確認 modal
     function openNewChatConfirmModal(){
         document.getElementById("newChatConfirmModal").showModal();
@@ -357,7 +248,7 @@
     function newChat(){
         console.log("newChat");
         // 關閉再確認 modal
-        closeNewChatConfirmModal();
+        closeModal_newChatConfirmModal();
         // 清空對話錄
         messages.splice(0, messages.length);
         chat_room_uuid.value = "INIT";
@@ -366,31 +257,29 @@
         chat();
     }
     // 關閉 NewChat 再確認 modal
-    function closeNewChatConfirmModal(){
+    function closeModal_newChatConfirmModal(){
         document.getElementById("newChatConfirmModal").close();
+    }
+    // 開啟 namePlanModal
+    function openModal_namePlanModal(){
+        planName.value = "";
+        document.getElementById("namePlanModal").showModal();
+    }
+    // 為 plan 命名
+    function namePlan(){
+        sumupPlan();
+    }
+    // 關閉 namePlanModal
+    function closeModal_namePlanModal(){
+        document.getElementById("namePlanModal").close();
     }
     // 關閉全部 modal
     function closeAllModal(){
-        closeNewChatConfirmModal();
-        closePromptModal();
+        closeModal_newChatConfirmModal();
+        closeModal_namePlanModal();
     }
 
     // 監聽
-    watch(promptAction, (newValue, oldValue) => {
-        combinePrompt();
-    });
-    watch(promptNation, (newValue, oldValue) => {
-        combinePrompt();
-    });
-    watch(promptScope, (newValue, oldValue) => {
-        combinePrompt();
-    });
-    watch(promptTime, (newValue, oldValue) => {
-        combinePrompt();
-    });
-    watch(promptType, (newValue, oldValue) => {
-        combinePrompt();
-    });
     watch(chatMode, (newValue, oldValue) => {
         console.log("watch.chatMode.newValue=" + newValue);
 
@@ -398,7 +287,7 @@
             newChat();
         }else{
             // 關閉再確認 modal
-            closeNewChatConfirmModal();
+            closeModal_newChatConfirmModal();
             // 清空對話錄
             messages.splice(0, messages.length);
         }
@@ -420,19 +309,19 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                     </svg>
                 </a>
-
-                <!-- 提詞機 -->
-                <a title="提詞機" class="cursor-pointer p-1 bg-yellow-500/50 text-gray-500 hover:text-gray-900 rounded-xl flex place-items-center" @click="openPromptModal">
-                    <svg class="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6h8m-8 6h8m-8 6h8M4 16a2 2 0 1 1 3.321 1.5L4 20h5M4 5l2-1v6m-2 0h4"/>
-                    </svg>
-                </a>        
             </div>
             <div class="flex-1 p-1">
                 <input v-if="userMessage.length <= 50" type="text" class="input w-1/1 h-1/1 rounded-xl border" v-model="userMessage" placeholder="想說點什麼呢?" :disabled="chatState === 'TALKING'" />
                 <textarea v-if="userMessage.length > 50" class="textarea w-1/1 h-1/1 rounded-xl border" v-model="userMessage" placeholder="想說點什麼呢?" :disabled="chatState === 'TALKING'"></textarea>
             </div>
             <div class="flex-none p-1 flex flex-row gap-1">
+                <!-- 統整 -->
+                <a v-if="chatState !== 'TALKING'" title="統整" class="cursor-pointer p-1 bg-blue-500/50 text-gray-500 hover:text-gray-900 rounded-xl flex place-items-center" @click="openModal_namePlanModal">
+                    <svg class="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11v5m0 0 2-2m-2 2-2-2M3 6v1a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Zm2 2v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8H5Z"/>
+                    </svg>
+                </a>
+
                 <!-- 傳送 -->
                 <a title="傳送" class="cursor-pointer p-1 bg-blue-500/50 text-gray-500 hover:text-gray-900 rounded-xl flex place-items-center" @click="send">
                     <span v-if="chatState === 'TALKING'" class="loading loading-spinner loading-md"></span>
@@ -498,58 +387,6 @@
 </div>
 
 
-<!-- prompt modal -->
-<dialog id="promptModal" class="modal modal-end">
-    <div class="modal-box h-1/1 w-4/5 flex flex-col bg-neutral-100">
-        <div class="flex flex-col justify-center">
-            <span class="text-lg text-gray-900 text-center">聊天提詞機</span>
-            <div class="divider divider-primary"></div>
-        </div>
-        <div class="w-1/1 flex flex-col overflow-y-auto gap-2">
-            <div class="w-1/1 flex flex-row items-center gap-1">
-                <span class="flex-none">國家:</span>
-                <select class="flex-1 border rounded-xl p-2" v-model="promptNation">
-                    <option v-for="(nObj, n_i) in promptOptions.nation" :value="nObj.value">{{ nObj.text }}</option>
-                </select>
-            </div>
-            <div class="w-1/1 flex flex-row items-center gap-1">
-                <span class="flex-none">主題:</span>
-                <select class="flex-1 border rounded-xl p-2" v-model="promptScope">
-                    <option v-for="(sObj, s_i) in promptOptions.scope" :value="sObj.value">{{ sObj.text }}</option>
-                </select>
-            </div>
-            <div class="w-1/1 flex flex-row items-center gap-1">
-                <span class="flex-none">類型:</span>
-                <select class="flex-1 border rounded-xl p-2" v-model="promptType">
-                    <option v-for="(tObj, t_i) in promptOptions.type" :value="tObj.value">{{ tObj.text }}</option>
-                </select>
-            </div>
-            <div class="w-1/1 flex flex-row items-center gap-1">
-                <span class="flex-none">時間:</span>
-                <select class="flex-1 border rounded-xl p-2" v-model="promptTime">
-                    <option v-for="(tObj, t_i) in promptOptions.time" :value="tObj.value">{{ tObj.text }}</option>
-                </select>
-            </div>
-
-            <textarea class="textarea w-1/1 h-40 mt-1 bg-gray-900 text-gray-100" placeholder="想說點什麼嗎??" v-model="prompt"></textarea>
-        </div>
-
-        <div class="divider divider-primary"></div>
-        <div class="modal-action px-10">
-            <button class="btn w-1/2 bg-gray-900 text-gray-200 hover:bg-yellow-300 hover:text-gray-900" @click.stop="closePromptModal">
-                關閉
-            </button>
-
-            <button class="btn w-1/2 bg-gray-200 text-gray-900 hover:bg-yellow-300" @click.stop="sendPrompt">
-                傳送
-            </button>
-        </div>
-    </div>
-    <form method="dialog" class="modal-backdrop">
-        <button>close</button>
-    </form>
-</dialog>
-
 <!-- newChatConfirm modal -->
 <dialog id="newChatConfirmModal" class="modal">
     <div class="modal-box h-3/10 w-1/1 flex flex-col bg-neutral-500">
@@ -559,7 +396,7 @@
 
         <div class="divider divider-primary"></div>
         <div class="modal-action px-10">
-            <button class="btn w-1/2 text-gray-200 bg-gray-900 hover:bg-yellow-200 hover:text-gray-900" @click="closeNewChatConfirmModal">
+            <button class="btn w-1/2 text-gray-200 bg-gray-900 hover:bg-yellow-200 hover:text-gray-900" @click="closeModal_newChatConfirmModal">
                 話題繼續
             </button>
 
@@ -573,6 +410,30 @@
     </form>
 </dialog>
 
+<!-- namePlanModal -->
+<dialog id="namePlanModal" class="modal">
+    <div class="modal-box h-3/10 w-1/1 flex flex-col bg-gray-200">
+        <div class="w-1/1 text-center text-black font-black bg-transparent rounded-xl">
+            <span class="text-2xl">請幫訓練計劃取個名字.</span>
+        </div>
+
+        <input class="input w-1/1 border border-1 border-black p-2" type="text" v-model="planName" />
+
+        <div class="divider divider-primary"></div>
+        <div class="modal-action px-10">
+            <button class="btn w-1/2 text-gray-200 bg-gray-900 hover:bg-yellow-200 hover:text-gray-900" @click="closeModal_namePlanModal">
+                取消
+            </button>
+
+            <button class="btn w-1/2 text-gray-900 bg-gray-200 hover:bg-yellow-200 hover:text-gray-900" @click="namePlan">
+                確認
+            </button>
+        </div>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
 
 </template>
 
