972bffbd16bc8c5fc8036f28202ac2a6803d2a11
advtek02.tn <advtek02.tn@innolux.com>
2026-01-15 16:54:53 +0800

    完善"刪除計劃"功能

--- src/components/FitnessPlan.vue
+++ src/components/FitnessPlan.vue
@@ -19,7 +19,8 @@
     // 已排定的訓練計劃
     let planList = reactive([]);
     // 選擇要刪除的計畫
-    let selRemoveObj = reactive({});
+    let selRemove_id = ref("");
+    let selRemove_planName = ref("");
 
     // 初始化 component
     function init(){
@@ -43,11 +44,45 @@
         Promise.all([fetchPromise_plan]).then((values) => {
             console.log("fetchInitData.values=", values);
 
-            
+            values[0].forEach((planObj, plan_i) => {
+                planList.push(planObj);
+            });
 
         });
     }
     
+    // 開啟 removeConfirmModal
+    function openModal_removeConfirmModal(selPlanObj){
+        console.log("openModal_removeConfirmModal.selPlanObj=", selPlanObj);
+
+        selRemove_id.value = selPlanObj["id"];
+        selRemove_planName.value = selPlanObj["plan_name"];
+        document.getElementById("removeConfirmModal").showModal();
+    }
+    // 執行刪除
+    function doRemove(planObj){
+        let removePromise = fetchData({
+            api: "delete_fitness_plan",
+            data: {
+                id: selRemove_id.value,
+            }
+        });
+        Promise.all([removePromise]).then((values) => {
+            console.log("removePromise.values=", values);
+
+            if(values[0]["result"] === true){
+                fetchInitData();
+                emit('popupMessage', true, "已完成刪除"); // Emitting the event with data
+                closeModal_removeConfirmModal();
+            }else{
+                emit('popupMessage', false, values[0]["message"]); // Emitting the event with data
+            }
+        });
+    }
+    // 關閉 removeConfirmModal
+    function closeModal_removeConfirmModal(){
+        document.getElementById("removeConfirmModal").close();
+    }
 
 </script>
 
@@ -58,11 +93,14 @@
         <li v-for="(planObj, plan_i) in planList" class="list-row hover:bg-yellow-100 items-center">
             <div class="text-4xl font-thin opacity-30 tabular-nums">{{ ((plan_i + 1) < 10 ? "0" : "") + (plan_i + 1) }}</div>
             <div class="list-col-grow">
-                <div class="text-lg">
-                    {{ "123" }}
+                <div class="text-xl underline">
+                    {{ planObj.plan_name }}
+                </div>
+                <div class="text-xs">
+                    {{ planObj.create_time }}
                 </div>
             </div>
-            <a class="cursor-pointer text-red-500 hover:text-red-900" title="刪除計劃">
+            <a class="cursor-pointer text-red-500 hover:text-red-900" title="刪除計劃" @click="openModal_removeConfirmModal(planObj)">
                 <svg class="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
@@ -87,13 +125,17 @@
         </div>
         <div class="divider divider-error"></div>
 
+        <div class="w-1/1 text-center text-xl">
+            {{ selRemove_planName }}
+        </div>
+
         <div class="divider divider-error"></div>
         <div class="modal-action gap-2 px-5">
-            <button class="btn w-1/2 bg-gray-900 text-gray-200 hover:bg-yellow-200 hover:text-gray-900">
+            <button class="btn w-1/2 bg-gray-900 text-gray-200 hover:bg-yellow-200 hover:text-gray-900" @click="closeModal_removeConfirmModal">
                 取消
             </button>
 
-            <button class="btn w-1/2 bg-gray-400 text-gray-900 hover:bg-yellow-200 " >
+            <button class="btn w-1/2 bg-gray-400 text-gray-900 hover:bg-yellow-200 " @click="doRemove" >
                 確認
             </button>
         </div>
